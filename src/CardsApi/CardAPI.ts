ec9431d7ffa3cf6c4d5d124219e759df48ded8f7
Pavelnagai <pavel.nagai@mail.ru>
2022-05-04 13:26:53 +0300

    addCard, getCard, deleteCard

--- src/CardsApi/CardAPI.ts
+++ src/CardsApi/CardAPI.ts
@@ -2,17 +2,75 @@ import {instance} from "./instance";
 import {AddCardsType, GetCardsType} from "./Api";
 
 
-export  const CardAPI = {
-    getCards(params?: GetCardsType ) {
+export const CardAPI = {
+    getCards(params?: GetCardsType) {
         return instance.get('/cards/pack', {params})
     },
     addCards(data: AddCardsType) {
         return instance.post('/cards/pack', data)
     },
-    deleteCard(id: any) {
+    deletePack(id: any) {
         return instance.delete('/cards/pack', {params: {id}})
     },
+    getCard(params?: GetCardType) {
+        return instance.get<GetCardRequestType>('/cards/card', {params})
+    },
+    addCard(data: AddCardResponseType) {
+        return instance.post<RequestAddType>('/cards/card', data)
+    },
+    deleteCard(id: string) {
+        return instance.delete('/cards/card', {params: id})
+    }
 }
+export type CardType = {
+    cardsPack_id: string
+    question?: string
+    answer?: string
+    grade?: number
+    shots?: number
+    answerImg?: string
+    questionImg?: string
+    questionVideo?: string
+    answerVideo?: string
+}
+export type AddCardResponseType = {
+    card: CardType
+}
+export type RequestAddType = {
+    newCard: CardType
+}
+
+export type GetCardType = {
+    cardAnswer?: string
+    cardQuestion?: string
+    cardsPack_id?: string
+    min?: number
+    max?: number
+    sortCards?: number
+    page?: number
+    pageCount?: number
+}
+export type GetCardRequestCardsArrayType = {
+    answer: string
+    question: string
+    cardsPack_id: string
+    grade: number
+    shots: number
+    user_id: string
+    created: string
+    updated: string
+    _id: string
+}
+export type GetCardRequestType = {
+    cards: Array<GetCardRequestCardsArrayType>
+    cardsTotalCount: number
+    maxGrade: number
+    minGrade: number
+    page: number
+    pageCount: number
+    packUserId: string
+}
+
 
 
 
